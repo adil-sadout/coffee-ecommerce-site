f3c8fe55342b53c313d7eeed3daa0140e76fb20a
Adil Sadout <adelsadout@hotmail.com>
2022-02-20 05:34:58 +0100

    changed text in about us

--- src/route/About.js
+++ src/route/About.js
@@ -5,8 +5,8 @@ function About() {
     <>
       <div className=" bg-silver d-md-flex justify-content-center align-items-center pb-5 px-5 ">
         <div className="p-2 pt-5 w-md-50 text-center text-md-start">
-          <h2 className="display-2">Coffee Shopping Simplified</h2>
-          <p>Order your groceries from SuperM with our easy to use app, and get your products delivered straight to your doorstep.</p>
+          <h2 className="display-2">About Us</h2>
+          <p>Better Coffee Sho offers a unique coffee house environment unlike any other in Macon. We are not only a place to drop in and get your morning cup of coffee (although you are more than welcome to do that), we are a place where you can sit down and enjoy that tailor-made cup of coffee.</p>
         </div>
         <div className="d-md-block d-none">
           <img src="assets/coffeebeans5.png" alt="coffee cup" className="rounded minmaxWidth" width="100%" />
